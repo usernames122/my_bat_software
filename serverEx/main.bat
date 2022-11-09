0d7c0a9aaa144cb74b63a71c8e7dbf46580798e3
usernames122 <88596366+usernames122@users.noreply.github.com>
2022-11-09 14:05:21 +0100

    Update main.bat

--- serverEx/main.bat
+++ serverEx/main.bat
@@ -1,3 +1,6 @@
+@echo off
+set "origpath=%CD%"
+:begin
 set "paperURL=https://serverjars.com/api/fetchJar/servers/paper/"
 set "spigotURL=https://serverjars.com/api/fetchJar/servers/spigot/"
 set "oldURL=https://serverjars.com/api/fetchJar/servers/spigot/1.8.8"
@@ -7,31 +10,53 @@ set "fURL=https://serverjars.com/api/fetchJar/modded/forge"
 CHOICE /C ABCDEF /M "Select [A] Paper Latest, [B] Spigot Latest, [C] Spigot 1.8.8, [D] Vanilla Latest, [E] Forge 1.12.2, [F] Forge Latest"
 IF %ERRORLEVEL% EQU 1 (
 set /p to="Where to put the JAR file?"
-cd /d "%to%"
-wget -t 40 -o server.jar "%paperURL%"
+
+wget –show-progress option -q -t 40 -o server.jar "%paperURL%"
+cd /d "%origpath%"
+robocopy index.html "%to%/server.jar"
+del index.html
+goto begin
 )
 IF %ERRORLEVEL% EQU 2 (
 set /p to="Where to put the JAR file?"
-cd /d "%to%"
-wget -t 40 -o server.jar "%spigotURL%"
+wget –show-progress option -q -t 40 -o server.jar "%oldURL%"
+cd /d "%origpath%"
+robocopy index.html "%to%/server.jar"
+del index.html
+goto begin
 )
 IF %ERRORLEVEL% EQU 3 (
 set /p to="Where to put the JAR file?"
-cd /d "%to%"
-wget -t 40 -o server.jar "%oldURL%"
+wget –show-progress option -q -t 40 -o server.jar "%oldURL%"
+cd /d "%origpath%"
+robocopy index.html "%to%/server.jar"
+del index.html
+goto begin
 )
 IF %ERRORLEVEL% EQU 4 (
 set /p to="Where to put the JAR file?"
-cd /d "%to%"
-wget -t 40 -o server.jar "%vanillaURL%"
+
+wget –show-progress option -q -t 40 -o server.jar "%vanillaURL%"
+cd /d "%origpath%"
+robocopy index.html "%to%/server.jar"
+del index.html
+goto begin
 )
 IF %ERRORLEVEL% EQU 5 (
 set /p to="Where to put the JAR file?"
-cd /d "%to%"
-wget -t 40 -o server.jar "%foldURL%"
+
+wget –show-progress option -q -t 40 -o server.jar "%foldURL%"
+cd /d "%origpath%"
+robocopy index.html "%to%/server.jar"
+del index.html
+goto begin
 )
 IF %ERRORLEVEL% EQU 6 (
 set /p to="Where to put the JAR file?"
-cd /d "%to%"
-wget -t 40 -o server.jar "%fURL%"
+
+wget –show-progress option -q -t 40 -o server.jar "%fURL%"
+cd /d "%origpath%"
+robocopy index.html "%to%/server.jar"
+del index.html
+goto begin
 )
